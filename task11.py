a7beec5078785c3972a9e9d86e1e14cc04017477
Vlad solo <solo.vlad55555@icloud.com>
2025-01-14 10:53:55 +0300

    сделал

---
+++ task11.py
@@ -0,0 +1,16 @@
+volume1 = 10
+volume2 = 20
+
+
+mass1 = 5
+mass2 = 10
+
+
+density1 = mass1 / volume1
+density2 = mass2 / volume2
+
+
+higher_density = max(density1, density2)
+
+
+print("Телo с большей плотностью:", higher_density)
