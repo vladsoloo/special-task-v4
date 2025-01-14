25628c0afcd9c2b2e1af6b0b1fe528d94a328b32
Vlad solo <solo.vlad55555@icloud.com>
2025-01-14 10:47:47 +0300

    сделал

---
+++ task7.py
@@ -0,0 +1,9 @@
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+max_value = max(a, b)
+
+min_value = min(a, b)
+
+print("Максимальное значение:", max_value)
+print("Минимальное значение:", min_value)
