16e318a48ff8eed1ec8306e06966025eb94bc177
Vlad solo <solo.vlad55555@icloud.com>
2025-01-14 10:51:24 +0300

    сделал

---
+++ task8.py
@@ -0,0 +1,13 @@
+distance_km = 10
+
+
+distance_ft = 30
+
+
+distance_m = distance_ft * 0.305
+
+
+smaller_distance = min(distance_km, distance_m)
+
+
+print("Меньшее расстояние:", smaller_distance)
