5eb415266f311de073cac2b92c1a62e3fa5a75da
Vlad solo <solo.vlad55555@icloud.com>
2025-01-14 10:53:00 +0300

    сделал

---
+++ task10.py
@@ -0,0 +1,11 @@
+radius = 5
+
+side = 8
+
+circle_area = 3.14159 * radius**2
+
+square_area = side**2
+
+largest_area = max(circle_area, square_area)
+
+print("Фигура с большей площадью:", largest_area)
