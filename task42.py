42b71d3a7638f7f447c1fa3f32c235615aef7285
wiergushtrefa <colbert_theht@bk.ru>
2025-01-16 13:09:02 +0300

    сделал

---
+++ task42.py
@@ -0,0 +1,14 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+c = int(input("Введите число c: "))
+
+
+if b > a > c:
+    print("True")
+else:
+    print("False")
+
+if b < a < c:
+    print("True")
+else:
+    print("False")
