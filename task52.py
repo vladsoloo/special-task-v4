ce167533f1a0de187ee50ede1c7e42d9733eea11
Vlad solo <solo.vlad55555@icloud.com>
2025-01-17 09:04:53 +0300

    сделал

---
+++ task52.py
@@ -0,0 +1,8 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+d = int(input("Введите число c: "))
+
+if d + 2 >= a and d + 2 <= b:
+    print('YES')
+else:
+    print('NO')
