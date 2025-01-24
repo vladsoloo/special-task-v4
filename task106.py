4f6f14bb69a51c1231e321c40864a19c7b01dc24
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-01-24 08:34:25 +0000

    сделал

--- task106.py
+++ task106.py
@@ -4,5 +4,5 @@ season = {'winter': [12, 1, 2], 'spring': [3, 4, 5], 'summer': [6, 7, 8],
           'autumn': [9, 10, 11]}
 
 for k, i in season.items():
-        if month in i:
-                print(k)
+    if month in i:
+        print(k)
