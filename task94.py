52051cbf37e6ceebac99229b7ec3c78d9504ba7a
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-01-24 06:36:08 +0000

    сделал

---
+++ task94.py
@@ -0,0 +1,9 @@
+a = int(input('Comand №1: '))
+b = int(input('Comand №2: '))
+
+if a == b:
+    print('Ничья')
+elif a < b:
+    print('Проигрыш команды №1')
+elif a > b:
+    print('Победа команды №1')
